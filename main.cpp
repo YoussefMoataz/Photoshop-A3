0a48bc015afa8d4f433efc12d173912612f3ccf2
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-03-31 12:00:45 +0200

    Added the library and images.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "bmplib.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
