5abd03f3dd6129b26b5c70cfe1db558634ae8527
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-04-03 18:05:46 +0200

    Functions done : {1, 2, 3, 5, 6, 8, 9, b, c} Added BlackAndWhite.

--- main.cpp
+++ main.cpp
@@ -202,7 +202,7 @@ void darkenAndLightenImage() {
 
 }
 
-// Action 8
+// Action 8 - Seif
 void enlargeImage() {
 
         
@@ -283,7 +283,7 @@ void enlargeImage() {
 
 }
 
-// Action b
+// Action b - Seif
 void shuffleImage() {
     
     string quarters;
@@ -663,22 +663,34 @@ void blurImage() {
 
 }
 
-// Action 1
+// Action 1 - Mohamed
 void blackAndWhiteFilter() {
+
+    for (int i = 0; i < SIZE; i++) {
+
+        for (int j = 0; j < SIZE; j++) {
+            if (image[i][j] > 127) {
+                image[i][j] = 255;
+            } else {
+                image[i][j] = 0;
+            }
+        }
+
+    }
 
 }
 
-// Action 4
+// Action 4 - Mohamed
 void flipImage() {
 
 }
 
-// Action 7
+// Action 7 - Mohamed
 void detectImageEdges() {
 
 }
 
-// Action a
+// Action a - Mohamed
 void mirrorImage() {
 
 }
